2cfea8669805545fdf1d0148596253f800f13065
Brian <rinkb@wwu.edu>
2025-05-15 09:52:37 -0700

    progress on changing split_data

--- split_data.py
+++ split_data.py
@@ -13,24 +13,22 @@ note: the test set percentage will be inferred as 1 - percent_train - percent_de
 '''
 
 if __name__ == "__main__":
-    if (len(sys.argv) != 7):
-        print("usage: split_data.py in out_train out_dev out_test percent_train percent_dev")
+    if (len(sys.argv) < 6):
+        print("usage: split_data.py in out_train out_dev percent_train percent_dev [out_test]")
         exit(1)
     input_filename = sys.argv[1]
     train_filename = sys.argv[2]
     dev_filename = sys.argv[3]
-    test_filename = sys.argv[4]
-    train_portion = float(sys.argv[5])
-    dev_portion = float(sys.argv[6])
+    train_portion = float(sys.argv[4])
+    dev_portion = float(sys.argv[5])
+    if (len(sys.argv) == 7):
+        test_filename = sys.argv[6]
 
     lst = manage_csv.create_data_entry_list(input_filename)
     random.shuffle(lst)
 
-    dev_idx = int(len(lst) * train_portion)
-    test_idx = dev_idx + int(len(lst) * dev_portion)
-    train_list = lst[:dev_idx]
-    dev_list = lst[dev_idx:test_idx]
-    test_list = lst[test_idx:]
+    '''if (len(sys.argv) == 7):
+        test_'''
 
     manage_csv.create_csv_from_entries(train_list, train_filename)
     manage_csv.create_csv_from_entries(dev_list, dev_filename)
